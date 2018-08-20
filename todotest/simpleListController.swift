5316df4cf79b0ecb9cdf39b7c256268883688bb0
sasakazu <sasakura.company@gmail.com>
2018-08-20 18:49:25 +0900

    little....

--- todotest/simpleListController.swift
+++ todotest/simpleListController.swift
@@ -18,24 +18,20 @@ class simpleListController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-         self.navigationItem.leftBarButtonItem = self.editButtonItem
+        self.navigationItem.leftBarButtonItem = self.editButtonItem
+    
     }
 
     
-    //viewDidLoadが呼ばれない可能性があるため
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        //保存されたelementsを取り出す
         if UserDefaults.standard.object(forKey: "elementsOfArray") != nil {
             showedArray = UserDefaults.standard.object(forKey: "elementsOfArray") as! [String]
         }
         
         tableView.reloadData()
+        
     }
     
     
@@ -63,7 +59,7 @@ class simpleListController: UITableViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if (segue.identifier == "showSecondView") {
             let vc2: detailViewController = (segue.destination as? detailViewController)!
-            // ViewControllerのtextVC2にメッセージを設定
+           
             vc2.text = selectedText!
           
         }
@@ -73,18 +69,18 @@ class simpleListController: UITableViewController {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+     
     }
 
-    // MARK: - Table view data source
+  
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
+       
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+    
         return showedArray.count
     }
 
@@ -103,66 +99,20 @@ class simpleListController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
-            //elementsの各cellについてdeleteする配列番号の消去
+           
             showedArray.remove(at: indexPath.row)
-            //その配列を再び保存
+           
             UserDefaults.standard.set(showedArray, forKey: "elementsOfArray")
-            //TableViewの更新
+           
             tableView.reloadData()
             
         } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+           
         }
     }
     
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
- 
-
+   
 }
 
 
